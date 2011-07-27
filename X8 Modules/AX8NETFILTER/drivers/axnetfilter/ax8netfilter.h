3680113c9bdb3d794cbf7453f1383a2d6a4bcf97
AnDyXX <a.kurzeja@gmail.com>
2011-07-27 11:54:36 +0200

    WIP

---
+++ X8 Modules/AX8NETFILTER/drivers/axnetfilter/ax8netfilter.h
@@ -0,0 +1,8 @@
+#ifndef __AX8NETFILTER_H__
+#define __AX8NETFILTER_H__
+
+extern struct kmem_cache ** ax8netfilter_skbuff_head_cache __read_mostly;
+extern struct kmem_cache ** ax8netfilter_skbuff_fclone_cache __read_mostly;
+
+
+#endif
