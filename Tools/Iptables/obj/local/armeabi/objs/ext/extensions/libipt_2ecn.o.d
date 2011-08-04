ff2a9644578a04c16bdb48f68bb4f059ca3deac1
AnDyXX <a.kurzeja@gmail.com>
2011-08-04 21:47:07 +0200

    iptables start

---
+++ Tools/Iptables/obj/local/armeabi/objs/ext/extensions/libipt_2ecn.o.d
@@ -0,0 +1,173 @@
+/home/andyx/android/X8/Tools/Iptables/obj/local/armeabi/objs/ext/extensions/libipt_2ecn.o: \
+ /home/andyx/android/X8/Tools/Iptables/jni/extensions/libipt_2ecn.c \
+ /opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/stdio.h \
+ /opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/sys/cdefs.h \
+ /opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h \
+ /opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/sys/_types.h \
+ /opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/machine/_types.h \
+ /opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/sys/types.h \
+ /opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/stdint.h \
+ /opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/linux/posix_types.h \
+ /opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/linux/stddef.h \
+ /opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/linux/compiler.h \
+ /opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/asm/posix_types.h \
+ /opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/asm/types.h \
+ /opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/linux/types.h \
+ /opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/machine/kernel.h \
+ /opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h \
+ /opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/string.h \
+ /opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/malloc.h \
+ /opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/stdlib.h \
+ /opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/alloca.h \
+ /opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/strings.h \
+ /opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/memory.h \
+ /opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/getopt.h \
+ /home/andyx/android/X8/Tools/Iptables/jni/include/iptables.h \
+ /home/andyx/android/X8/Tools/Iptables/jni/include/iptables_common.h \
+ /home/andyx/android/X8/Tools/Iptables/jni/include/libiptc/libiptc.h \
+ /home/andyx/android/X8/Tools/Iptables/jni/include/libiptc/ipt_kernel_headers.h \
+ /opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/limits.h \
+ /opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/sys/limits.h \
+ /opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/linux/limits.h \
+ /opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/machine/internal_types.h \
+ /opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/machine/limits.h \
+ /opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/sys/syslimits.h \
+ /opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/sys/socket.h \
+ /opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/linux/socket.h \
+ /opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/asm/socket.h \
+ /opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/asm/sockios.h \
+ /opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/linux/sockios.h \
+ /opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/linux/uio.h \
+ /opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/linux/ip.h \
+ /opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/asm/byteorder.h \
+ /opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/linux/byteorder/little_endian.h \
+ /opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/linux/byteorder/swab.h \
+ /opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/linux/byteorder/generic.h \
+ /opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/linux/in.h \
+ /opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/linux/if.h \
+ /opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/linux/hdlc/ioctl.h \
+ /opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/linux/icmp.h \
+ /opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/linux/tcp.h \
+ /opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/linux/udp.h \
+ /opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/linux/in6.h \
+ /opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/linux/netfilter_ipv4/ip_tables.h \
+ /opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/linux/netfilter_ipv4.h \
+ /opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/linux/netfilter.h \
+ /opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/linux/netfilter/x_tables.h \
+ /opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/linux/netfilter/xt_tcpudp.h \
+ /home/andyx/android/X8/Tools/Iptables/jni/include/linux/netfilter_ipv4/ipt_2ecn.h \
+ /opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/linux/netfilter_ipv4/ipt_dscp_.h
+
+/opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/stdio.h:
+
+/opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/sys/cdefs.h:
+
+/opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/sys/_types.h:
+
+/opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/machine/_types.h:
+
+/opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/sys/types.h:
+
+/opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/stdint.h:
+
+/opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/linux/posix_types.h:
+
+/opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/linux/stddef.h:
+
+/opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/linux/compiler.h:
+
+/opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/asm/posix_types.h:
+
+/opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/asm/types.h:
+
+/opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/linux/types.h:
+
+/opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/machine/kernel.h:
+
+/opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h:
+
+/opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/string.h:
+
+/opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/malloc.h:
+
+/opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/stdlib.h:
+
+/opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/alloca.h:
+
+/opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/strings.h:
+
+/opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/memory.h:
+
+/opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/getopt.h:
+
+/home/andyx/android/X8/Tools/Iptables/jni/include/iptables.h:
+
+/home/andyx/android/X8/Tools/Iptables/jni/include/iptables_common.h:
+
+/home/andyx/android/X8/Tools/Iptables/jni/include/libiptc/libiptc.h:
+
+/home/andyx/android/X8/Tools/Iptables/jni/include/libiptc/ipt_kernel_headers.h:
+
+/opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/limits.h:
+
+/opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/sys/limits.h:
+
+/opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/linux/limits.h:
+
+/opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/machine/internal_types.h:
+
+/opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/machine/limits.h:
+
+/opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/sys/syslimits.h:
+
+/opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/sys/socket.h:
+
+/opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/linux/socket.h:
+
+/opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/asm/socket.h:
+
+/opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/asm/sockios.h:
+
+/opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/linux/sockios.h:
+
+/opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/linux/uio.h:
+
+/opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/linux/ip.h:
+
+/opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/asm/byteorder.h:
+
+/opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/linux/byteorder/little_endian.h:
+
+/opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/linux/byteorder/swab.h:
+
+/opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/linux/byteorder/generic.h:
+
+/opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/linux/in.h:
+
+/opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/linux/if.h:
+
+/opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/linux/hdlc/ioctl.h:
+
+/opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/linux/icmp.h:
+
+/opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/linux/tcp.h:
+
+/opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/linux/udp.h:
+
+/opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/linux/in6.h:
+
+/opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/linux/netfilter_ipv4/ip_tables.h:
+
+/opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/linux/netfilter_ipv4.h:
+
+/opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/linux/netfilter.h:
+
+/opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/linux/netfilter/x_tables.h:
+
+/opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/linux/netfilter/xt_tcpudp.h:
+
+/home/andyx/android/X8/Tools/Iptables/jni/include/linux/netfilter_ipv4/ipt_2ecn.h:
+
+/opt/android-ndk-r5c/platforms/android-3/arch-arm/usr/include/linux/netfilter_ipv4/ipt_dscp_.h:
