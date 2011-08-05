2b595963139ccbbb3e4f9463517cc8646f060cf2
AnDyXX <a.kurzeja@gmail.com>
2011-08-05 08:21:55 +0200

    WIP

--- Tools/Iptables/obj/local/armeabi/objs/ext/extensions/initext.o.d
+++
@@ -1,5 +0,0 @@
-/home/andyx/android/X8/Tools/Iptables/obj/local/armeabi/objs/ext/extensions/initext.o: \
- /home/andyx/android/X8/Tools/Iptables/jni/extensions/initext.c \
- /home/andyx/android/X8/Tools/Iptables/jni/extensions/gen_initext.c
-
-/home/andyx/android/X8/Tools/Iptables/jni/extensions/gen_initext.c:
